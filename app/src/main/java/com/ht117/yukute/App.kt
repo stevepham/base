28ea3a6868a0466a724d6ee1da7d4f6b1dc1c89e
Steve Pham <treatmaster5277@gmail.com>
2020-07-15 22:04:30 +0700

    Update base

--- app/src/main/java/com/ht117/yukute/App.kt
+++ app/src/main/java/com/ht117/yukute/App.kt
@@ -1,13 +1,22 @@
 package com.ht117.yukute
 
 import android.app.Application
+import com.ht117.yukute.di.getDI
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class App: Application() {
 
     override fun onCreate() {
         super.onCreate()
 
-
+        startKoin {
+            androidContext(this@App)
+            androidLogger(Level.DEBUG)
+            modules(getDI())
+        }
     }
 
 }
